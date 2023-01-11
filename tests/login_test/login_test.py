b6f215aa5eabfefd4f9050686a2f7cd67cf4c7dc
fernando <fernandoadt1@gmail.com>
2023-01-11 19:50:59 -0300

    Added one more login test

--- tests/login_test/login_test.py
+++ tests/login_test/login_test.py
@@ -1,4 +1,4 @@
-"""Module with first playwright test example."""
+"""Module with login tests"""
 import re
 from playwright.sync_api import Page, expect
 import pytest
@@ -8,12 +8,17 @@ from pages.login import LoginPage
 @pytest.mark.usefixtures('go_to_base_url')
 class TestLogin:
     def testUserCanLoginSuccessfully(self, page: Page, login_page: LoginPage):
-        page.goto(page.url)
-        # Expect a title "to contain" a substring.
         expect(page).to_have_title(re.compile('Swag Labs'))
-
         expect(page.locator('.login_logo')).to_be_visible()
 
         login_page.login('standard_user', 'secret_sauce')
 
-        expect(page.locator('.inventory_container')).to_be_visible()
+        expect(page.locator('.inventory_container')).to_be_visible() 
+        
+    def testLoginErrorWhenInvalidCredentials(self, page: Page, login_page: LoginPage):
+        expect(page).to_have_title(re.compile('Swag Labs'))
+        expect(page.locator('.login_logo')).to_be_visible()
+
+        login_page.login('invalid_user', 'invalid_password')
+
+        expect(page.locator('[data-test="error"]')).to_be_visible()
